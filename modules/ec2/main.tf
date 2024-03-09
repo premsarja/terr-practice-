992a3882f929084666a66a831796dbd54be8ec08
premsarja <premsagar@prodot.in>
2024-03-09 12:22:21 +0530

    done

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "ec2" {
   ami           = "ami-0f75a13ad2e340a58"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [var.sg]
+  vpc_security_group_ids = [var.prem]
 
   tags = {
     Name = "terr-1"
@@ -13,4 +13,4 @@ output "ip" {
   
 }
 
-variable "sg" {}
+variable "prem" {}
--- modules/main.tf
+++ modules/main.tf
@@ -1,6 +1,6 @@
 module "ec2" {
   source = "./ec2"
-  sg = module.sg.sgid
+  prem = module.sg.sgid
 }
 
 module "sg" {
